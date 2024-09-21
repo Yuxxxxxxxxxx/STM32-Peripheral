8c8619a151955eb866d7b2d8534b250c80c7dca6
Yuxxxxxxxxxx <2936481298@qq.com>
2024-09-21 14:32:19 +0800

    STM32F407+W25Q128

--- W25Q128JV/app/main.c
+++ W25Q128JV/app/main.c
@@ -10,13 +10,40 @@
 
 extern void board_lowlevel_init(void);
 
-uint8_t wr_data[128] = {0};
-uint8_t rd_data[128] = {0};
+uint8_t i = 0;
+
+void test_number(void)
+{	
+	uint8_t wr_data[128] = {0};
+    uint8_t rd_data[128] = {0};
+	
+	for (i = 0; i < 128; i++)
+	{
+		wr_data[i] = i;
+	}
+	w25q32_dev.wr(wr_data, 0, 128);
+
+	w25q32_dev.rd(rd_data, 0, 128); // 从falsh读取数据并打印
+	printf("\r\nread data is :\r\n");
+	for (i = 0; i < 128; i++)
+	{
+		printf("%d, ", rd_data[i]);
+	}
+}
+
+void test_string(void)
+{
+	uint8_t data[13] = "hello world!";
+	uint8_t rdata[4] = { 0 };
+	
+	w25q32_dev.wr(data, 0, sizeof(data));
+	w25q32_dev.rd(rdata, 0, sizeof(data));
+	
+	printf("%s", rdata);
+}
 
 int main(void)
 {
-	uint8_t i = 0;
-
 	board_lowlevel_init();
 	bl_delay_init();
 	bl_led_init();
@@ -30,18 +57,11 @@ int main(void)
 
 	printf("detact w25q128 ok!\r\n");
 	printf("write data !\r\n"); // 向flash写入数据
-	for (i = 0; i < 128; i++)
-	{
-		wr_data[i] = i;
-	}
-	w25q32_dev.wr(wr_data, 0, 128);
-
-	w25q32_dev.rd(rd_data, 0, 128); // 从falsh读取数据并打印
-	printf("\r\nread data is :\r\n");
-	for (i = 0; i < 128; i++)
-	{
-		printf("%d, ", rd_data[i]);
-	}
-
+	
+	
+	test_number();
+	
+	test_string();
+	
 	return 0;
 }
