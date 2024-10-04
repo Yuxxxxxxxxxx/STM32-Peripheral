f2a1f127aa8133d4b30e85412bfbee09e699ece1
Yuxxxxxxxxxx <2936481298@qq.com>
2024-10-04 16:53:18 +0800

    STM32F407 base template with led, key, uart

--- base-template/app/main.c
+++ base-template/app/main.c
@@ -1,9 +1,24 @@
 #include <stdbool.h>
 #include <stdint.h>
+#include <stdio.h>
 #include "stm32f4xx.h"
+#include "main.h"
+#include "led.h"
+#include "button.h"
+#include "console_uart.h"
+
+extern void board_lowlevel_init(void);
 
 int main(void)
 {
+	board_lowlevel_init();
+	
+	bl_delay_init();
+	bl_led_init();
+	bl_button_init();
+	bl_uart_init();
+	
 	while (1)
-		;
+	{
+	}
 }
